9c22e4b8f746c2907c3578796911ef5f2e582ee9
Likhach Alexander <smouzi45@gmail.com>
2020-10-11 00:29:41 +0300

    Stage 2!

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 package readability;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        if (str.length() > 100) {
-            System.out.println("HARD");
-        } else {
+        String[] sentences = str.split("[!.?]");
+        int total = 0;
+        for(String sentence: sentences) {
+            String[] words = sentence.strip().split(" ");
+            total += words.length;
+        }
+        float average = (float) total / sentences.length;
+        if (average <= 10f) {
             System.out.println("EASY");
+        } else {
+            System.out.println("HARD");
         }
     }
 }
