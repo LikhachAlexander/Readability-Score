3d6e757c8fb269ae9b076f5efc8ca212b4be8f8a
Likhach Alexander <smouzi45@gmail.com>
2020-11-04 20:48:15 +0300

    Improve word class

--- Word.java
+++ Word.java
@@ -3,6 +3,8 @@ package readability;
 public class Word {
     private final String word;
 
+    public static final char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'y'};
+
     public Word(String word) {
         this.word = word.strip();
     }
@@ -13,5 +15,40 @@ public class Word {
 
     public int getNumberOfCharacters() {
         return word.length();
+    }
+
+    public static boolean isVowel(char b) {
+        char a = Character.toLowerCase(b);
+        for (char vowel : vowels) {
+            if (a == vowel) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getNumberOfSyllables() {
+        int syllables = 0;
+        boolean isSyllable = false;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (isVowel(c) && !isSyllable) {
+                syllables++;
+                isSyllable = true;
+            } else if (!isVowel(c)){
+                isSyllable = false;
+            }
+        }
+        if (word.charAt(word.length() - 1) == 'e') {
+            syllables--;
+        }
+        if (syllables == 0) {
+            syllables++;
+        }
+        return syllables;
+    }
+
+    public boolean isPolysyllable() {
+        return getNumberOfSyllables() >= 3;
     }
 }
