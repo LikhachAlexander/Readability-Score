881c3d1baaee1ca9e580d3d2bfb149b7b927928f
Likhach Alexander <smouzi45@gmail.com>
2020-11-04 20:52:05 +0300

    Task completition

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,5 +14,10 @@ public class Main {
 
         TextAnalyzer analyzer = new TextAnalyzer(string);
         analyzer.printInfo();
+
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+        Scanner scanner = new Scanner(System.in);
+        String option = scanner.next();
+        analyzer.printScore(option);
     }
 }
